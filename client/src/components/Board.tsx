b5e13692affeade733a782fb8af33ae8e46f6e2e
Your Name <jeremykaizer@gmail.com>
2023-08-23 20:20:42 -0400

    Loading Agregado en componente Board.tsx

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,7 +1,33 @@
 import { FC, useEffect, useState } from "react";
 import { INIT_DATA_SUDOKU, getSudokuData } from "../services/sudokuApi";
-import { IBoard } from "../types/gameTypes";
+import '../styles/loader.css';
+import { IBoard, IBoardRender } from "../types/gameTypes";
 import { BoardElement } from "./BoardElement";
+
+const BoardRender: FC<IBoardRender> = ({board}) => {
+	return (
+		<div className="grid_sk gap-3">
+			{
+				board.unsolved.map((row, i) => (
+					<BoardElement
+						key={`boardElement-${i}`}
+						element={row}
+					/>
+				))
+			}
+		</div>
+	)
+}
+
+const Loading = () => {
+	return (
+		<div className="loader-bg">
+			<span className="loader">
+				<span className="loader-inner"></span>
+			</span>
+		</div>
+	)
+}
 
 export const Board: FC<IBoard> = ({ difficult }) => { 
 	const [gameBoard, setGameBoard] = useState(INIT_DATA_SUDOKU)
@@ -11,13 +37,11 @@ export const Board: FC<IBoard> = ({ difficult }) => {
 	}, [])
 
 	return (
-		<div className="grid_sk gap-3">
-			{gameBoard.unsolved.map((row, i) => (
-				<BoardElement
-					key={`boardElement-${i}`}
-					element={row}
-				/>
-			))}
+		<div>
+			{gameBoard.unsolved.length
+				? <BoardRender board={gameBoard} />
+				: <Loading />
+			}
 		</div>
 	);
 };
--- client/src/types/gameTypes.ts
+++ client/src/types/gameTypes.ts
@@ -1,8 +1,14 @@
+import { ISudokuData } from "./apiTypes";
+
 export type Difficult = "easy" | "normal" | "hard";
 export type BoardType = number[][]
 
 export interface IBoardElement {
 	element: number[]
+}
+
+export interface IBoardRender {
+	board: ISudokuData
 }
 
 export interface IBoard {
