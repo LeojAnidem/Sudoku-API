0ffac758348c50f1f4a1ef1690f2f361183ee610
Leoj Anidem <jeremykaizer@gmail.com>
2024-04-23 17:51:43 -0400

    [Client] Tiempo agregado

--- client/src/components/LifeComponent.tsx
+++ client/src/components/LifeComponent.tsx
@@ -1,17 +1,42 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { GameContext } from "../context/GameProvider"
 
 export const LifeComponent = () => {
   const {state} = useContext(GameContext)
+  
+  useEffect(() => {
+    
+  }, [])
+
+  const convertToTimer = (miliseconds: number) :string => {
+    let min = Math.floor(miliseconds / 60000)
+    let sec = ((miliseconds % 60000) / 1000).toFixed(0)
+    
+    return (
+      sec === '60'
+        ? (min + 1) + ":00" 
+        : min + ":" + (sec < '10' ? "0" : "") + sec
+    );
+  }
 
   return (
-    <h1
-      className="
-        text-tremor-brand-emphasis text-tremor-title
-        font-semibold
-      "
-    >
-      Vidas: {state.lifes}
-    </h1>
+    <div>
+      <h1
+        className="
+          text-tremor-brand-emphasis text-tremor-title
+          font-semibold
+        "
+      >
+        Vidas: {state.lifes}
+      </h1>
+      <h2
+        className="
+          text-tremor-brand-emphasis text-tremor-title
+          font-semibold
+        "
+      >
+        Tiempo: {convertToTimer(state.time)}
+      </h2>
+    </div>
   )
 }
