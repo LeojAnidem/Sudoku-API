249e0af917bb0e87310da5a606fd208d50e1e40e
Leoj Anidem <jeremykaizer@gmail.com>
2024-05-29 08:56:51 -0400

    [Client] Se movieron los archivos referente a las pantallas de gameOver y pausa a una nueva carpeta. Tambien se agrego la nueva pantalla de pausa (trabajo en progreso)

--- client/src/components/game/GameComponent.tsx
+++ client/src/components/game/GameComponent.tsx
@@ -2,13 +2,14 @@ import { useContext } from "react"
 import { Sudoku } from "./board/Sudoku"
 import { CountDownComponent } from "./options/CountDownComponent"
 import { DifficultTab } from "./options/DifficultTab"
-import { FailScreenComponent } from "./options/FailScreenComponent"
+import { FailScreenComponent } from "./screens/FailScreenComponent"
 import { LifeComponent } from "./options/LifeComponent"
 import { ScoreComponent } from "./options/ScoreComponent"
 import { GameContext } from "../../context/GameProvider"
 import { QuestionComponent } from "./options/QuestionComponent"
 import { PauseButton } from "./options/PauseButton"
 import { GameStatus } from "../../types/gameTypes"
+import { PauseScreenComponent } from "./screens/PauseScreenComponent"
 
 export const GameComponent = () => {
   const { state } = useContext(GameContext)
@@ -20,28 +21,24 @@ export const GameComponent = () => {
         justify-center gap-4
       "
     >
-      <div 
-        className="
-          w-full flex justify-between items-center
-        "
-      >
+      <div className="w-full flex justify-between items-center">
         <CountDownComponent />
         <PauseButton />
         <QuestionComponent />
         <ScoreComponent />
       </div>
-      <div
-        className="
-          flex justify-between items-center gap-2
-        "
-      >
+      <div className="flex justify-between items-center gap-2">
         <LifeComponent />
         <DifficultTab />
       </div>
       <Sudoku />
-      {state.status === GameStatus.gameOver 
-        ? <FailScreenComponent /> 
-        : <></>
+      {
+        state.status === GameStatus.gameOver &&
+        <FailScreenComponent /> 
+      }
+      {
+        state.status === GameStatus.pause &&
+        <PauseScreenComponent /> 
       }
     </div>
   )
---
+++ client/src/components/game/screens/PauseScreenComponent.tsx
@@ -0,0 +1,14 @@
+export const PauseScreenComponent = () => {
+  return (
+    <div
+			className="
+				w-full h-full bg-opacity-90 absolute top-0 p-6
+				flex justify-center items-center select-none
+				text-2xl text-white font-semibold
+				bg-dark-tremor-background z-[2]
+			"
+		>
+			<h2>Pause</h2>
+		</div>
+  )
+}
