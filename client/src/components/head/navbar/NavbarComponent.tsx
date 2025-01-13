19d5b9f5360689fe68edae5e540d95e6c2710e76
Leoj Anidem <jeremykaizer@gmail.com>
2025-01-13 08:39:33 -0500

    [Server] Vercel.json add!

--- client/src/components/head/navbar/NavbarComponent.tsx
+++ client/src/components/head/navbar/NavbarComponent.tsx
@@ -4,12 +4,12 @@ import '../styles/NavbarStyles.css'
 export const NavbarComponent = () => {
   return (
     <nav className='w-full h-full flex items-center justify-end gap-10'>
-      {/* <a className='navbar__link' href='#'>
+      <a className='navbar__link' href='#'>
         Play
       </a>
       <a className='navbar__link' href='#'>
         Explore API
-      </a> */}
+      </a>
       <a className='navbar__link' href='https://github.com/LeojAnidem'>
         <div className='navbar__icon'>
           <IconGitHub/>
