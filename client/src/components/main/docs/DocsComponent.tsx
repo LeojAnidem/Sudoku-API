a98c4bee07cc253bcfa456eec7d4f29fed29f05c
Leoj Anidem <jeremykaizer@gmail.com>
2025-09-23 17:25:35 -0400

    feat(client: components/main/docs): Add html structure for documentation section

--- client/src/components/main/docs/DocsComponent.tsx
+++ client/src/components/main/docs/DocsComponent.tsx
@@ -1,7 +1,46 @@
+import "./styles/DocsStyles.css"
+
 export const DocsComponent = () => {
   return (
-    <h1>
-      Docs
-    </h1>
+    <div className="docs">
+      <h1 className="docs__title">
+        Documentation
+      </h1>
+      <ul className="docs__list">
+        {/* structure for each item */}
+        <li className="docs__article">
+          <h2 className="docs__article__title">
+            Url Api
+          </h2>
+          <div className="docs__codeSpace">
+            <span className="docs__codeSpace__text">
+              space for code and url api
+            </span>
+            <button className="docs__codeSpace__btn">
+              icon for copy to clipboard
+            </button>
+          </div>
+          <p className="docs__article__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias in architecto blanditiis vitae perspiciatis animi doloremque velit temporibus ipsa dignissimos quisquam repellat, laudantium minima sint similique ullam non? Aperiam, eligendi.
+          </p>
+        </li>
+        <article className="docs__article">
+          <h2 className="docs__article__title">
+            Url Api
+          </h2>
+          <div className="docs__codeSpace">
+            <span className="docs__codeSpace__text">
+              space for code and url api
+            </span>
+            <button className="docs__codeSpace__btn">
+              icon for copy to clipboard
+            </button>
+          </div>
+          <p className="docs__article__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias in architecto blanditiis vitae perspiciatis animi doloremque velit temporibus ipsa dignissimos quisquam repellat, laudantium minima sint similique ullam non? Aperiam, eligendi.
+          </p>
+        </article>
+      </ul>
+    </div>
   )
 }
